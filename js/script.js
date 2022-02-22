d96e6ee98608a03ef758ef553f2ce81bfaed1b59
Salvatore1985 <80257763+Salvatore1985@users.noreply.github.com>
2022-02-22 15:57:57 +0100

    add toFixed

--- js/script.js
+++ js/script.js
@@ -17,13 +17,31 @@ console.log("Il tuo Cognome è :", userLastName);
 const userAge = parseInt(prompt("Quanti anni hai ?", "36"));
 console.log("I tuoi anni sono :", userAge);
 
-const userKms = parseInt(prompt("Quanti KM vuoi percorrere ?", "36"));
+const userKms = parseInt(prompt("Quanti KM vuoi percorrere ?", "10"));
 console.log("I Km che hai inserito sono :", userKms);
 
 //* VARIABILE 
 let userDiscount;
 let userPrice = 0.21;
 let sum = userPrice * userKms;
+
+
+// ! CONVALIDE
+if (userAge <= 18) {
+    sum *= 0.8;
+    userDiscount = "Essendo Minorenne hai diritto a uno sconto del 20%";
+} else if (userAge >= 65) {
+    sum *= 0.6;
+    userDiscount = "Essendo Over hai diritto a uno sconto del 60%";
+} else {
+    userDiscount = "Grazie per aver viaggiato con Trenitalia";
+
+};
+
+
+
+
+
 
 //* INSERISCO I DATI DEL DOM
 
@@ -32,11 +50,4 @@ elementLastName.innerHTML = userLastName;
 elementAge.innerHTML = userAge;
 elementKms.innerHTML = userKms;
 elementDiscount.innerHTML = userDiscount;
-elementPrice.innerHTML = sum;
-
-// ! CONVALIDE
-if (condition) {
-
-} else {
-
-}
+elementPrice.innerHTML = sum.toFixed(2);
