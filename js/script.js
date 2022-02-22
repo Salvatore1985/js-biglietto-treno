8d04ea94cb9e37bd4ca5e2f27cb20262f952eb9d
Salvatore1985 <80257763+Salvatore1985@users.noreply.github.com>
2022-02-22 15:31:26 +0100

    add js

--- js/script.js
+++ js/script.js
@@ -2,9 +2,10 @@
 
 let elementName = document.getElementById("name");
 let elementLastName = document.getElementById("last-name");
-let elementColor = document.getElementById("my-color");
-let elementPassword = document.getElementById("password-generator");
-
+let elementAge = document.getElementById("age");
+let elementKms = document.getElementById("kms");
+let elementDiscount = document.getElementById("discount");
+let elementPrice = document.getElementById("price");
 
 //* CHIEDO ALL'UTENTE DI INSERIRE I DATI
 const userName = prompt("Qual'è il tuo Nome ?", "Salvatore");
@@ -13,16 +14,23 @@ console.log("Il tuo Nome è :", userName);
 const userLastName = prompt("Qual'è il tuo Cognome ?", "Cascone");
 console.log("Il tuo Cognome è :", userLastName);
 
-const userColor = prompt("Qual'è il tuo colore preferito ?", "Verde");
-console.log("Il tuo Colore preferito è :", userColor);
+const userAge = prompt("Qual'è il tuo colore preferito ?", "36");
+console.log("Il tuo Colore preferito è :", userAge);
 
+const userKms = prompt("Qual'è il tuo colore preferito ?", "36");
+console.log("I Km che hai inserito sono :", userKms);
+
+//* VARIABILE 
+let userDiscount;
+let userPrice = 0.21;
+let sum = userPrice * userKms;
 
 //* INSERISCO I DATI DEL DOM
 
 elementName.innerHTML = userName;
 elementLastName.innerHTML = userLastName;
-elementColor.innerHTML = userColor;
-elementPassword.innerHTML = userName + userLastName + userColor + "21";
-
-console.log("La tua Password è :", elementPassword.innerHTML);
+elementAge.innerHTML = userAge;
+elementKms.innerHTML = userKms;
+elementDiscount.innerHTML = userDiscount;
+elementPrice.innerHTML = sum;
 
